174a21df5b20d3b626e8e36f48404e358a1c1716
redjax <no@none.com>
2025-03-14 14:58:35 -0400

    Add docstrings, comments to __main__ file

--- src/crawlbookmarks/__main__.py
+++ src/crawlbookmarks/__main__.py
@@ -1,3 +1,12 @@
+"""Scrape a bookmarks.html file exported from your browser and parse it into a JSON file.
+
+Description:
+    Loads a bookmarks.html file & scrapes it with BeautifulSoup. Discovered bookmarks are formatted
+    into a Python dict, which can be optionally saved to a JSON file.
+
+    The app can be run as a CLI by calling it directly, i.e. `python crawlbookmarks --help`.
+"""
+
 import logging
 
 from crawlbookmarks.main import run_cli
@@ -6,6 +15,7 @@ log = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     try:
+        ## Start the CLI
         run_cli()
     except Exception as exc:
         logging.basicConfig(
