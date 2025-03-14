744c995659b5aa63855844cf5c37e724af6c793c
redjax <no@none.com>
2025-03-14 15:25:23 -0400

    Add comments to controller class

--- src/crawlbookmarks/chrome/controllers.py
+++ src/crawlbookmarks/chrome/controllers.py
@@ -12,6 +12,15 @@ __all__ = ["ChromeBookmarksController"]
 
 
 class ChromeBookmarksController(AbstractContextManager):
+    """Controller class for Chrome bookmarks.
+
+    Params:
+        bookmarks_file (str): Path to the bookmarks HTML file to parse (default: bookmarks.html)
+        output_file (str): Path where the parsed bookmarks data will be saved (default: bookmarks.json)
+        save_results (bool): When `True`, results will be saved to the path in `output_file` (default: True)
+        include_separators (bool): When `True`, include separator bookmarks (default: False)
+    """
+
     def __init__(
         self,
         bookmarks_file: str = "bookmarks.html",
@@ -41,6 +50,7 @@ class ChromeBookmarksController(AbstractContextManager):
         return True
 
     def parse_bookmarks(self) -> dict:
+        """Parse bookmarks into dict and optionally save to file."""
         self.logger.debug(f"Parsing file '{self.bookmarks_file}'")
         try:
             bookmarks_data = chrome.parse_bookmarks(
@@ -58,6 +68,7 @@ class ChromeBookmarksController(AbstractContextManager):
         return bookmarks_data
 
     def save_bookmarks(self, bookmarks_data: dict) -> None:
+        """Save parsed bookmarks data to output file."""
         self.logger.debug(f"Exporting parsed bookmarks to {self.output_file}")
         try:
             io.export_to_json(bookmarks_data, self.output_file)
